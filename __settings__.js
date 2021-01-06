09515f0bbde8c5262a093ebc8f2f0971106ab9d3
ryoyen1 <43334112+ryoyen1@users.noreply.github.com>
2021-01-07 04:55:57 +0700

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1064519.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 40476716, 40478103, 40494954, 40494974, 40545627, 40570875, 40618892, 40619861, 40621950, 40622092 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/40478125/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/40478126/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/40478124/1/ammo.js', 'preload' : true},
+];
